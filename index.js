f584065c1357aa0d22c19b395649ecfe6f877c88
Tom Nielsen <tanielsen@gmail.com>
2024-08-07 12:30:30 +0100

    move code to onload

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Workflow = require("@saltcorn/data/models/workflow");
 const Form = require("@saltcorn/data/models/form");
 const Trigger = require("@saltcorn/data/models/trigger");
-//const cluster = require("cluster");
+const cluster = require("cluster");
 const NextcloudTalk = require("nctalkclient");
 const { interpolate } = require("@saltcorn/data/utils");
 
+let talk;
+let listofrooms;
 const configuration_workflow = () =>
   new Workflow({
     /* onDone: async (cfg) => {
@@ -53,11 +55,57 @@ const configuration_workflow = () =>
       },
     ],
   });
+const onLoad = async (cfg) => {
+  if (!cfg) return;
+  let { url, turn_port, system_username, system_password } = cfg;
+  talk = new NextcloudTalk({
+    server: url,
+    user: system_username,
+    pass: system_password,
+    port: turn_port,
+    debug: true,
+  });
+  talk.start(500);
+  talk.on("Error", (e) => {
+    console.error("Error Event ", e);
+  });
+
+  // Debug
+  talk.on("Debug", (e) => {
+    console.log("Debug Event ", e);
+  });
+  await new Promise((resolve, reject) => {
+    talk.on("Ready", (listofrooms1) => {
+      listofrooms = listofrooms1;
+      resolve();
+    });
+  });
+
+  if (!cluster.isMaster) return;
+
+  /*const { broker_url, subscribe_channels, is_json } = cfg;
+    if (client) await client.end();
+    const broker_url1 = broker_url.includes("://")
+      ? broker_url
+      : `mqtt://${broker_url}`;
+    client = mqtt.connect(broker_url1, { reconnectPeriod: 1000 });
+    client.on("connect", function () {
+      for (channel of subscribe_channels.split(","))
+        client.subscribe(channel.trim());
+    });
+    client.on("message", function (topic, message) {
+      //console.log("MQTT", topic, message);
+      const payload = is_json
+        ? JSON.parse(message.toString())
+        : message.toString();
+      Trigger.emitEvent("MqttReceive", topic, null, payload);
+    });*/
+};
 
 module.exports = {
   sc_plugin_api_version: 1,
   configuration_workflow,
-  //onLoad,
+  onLoad,
   actions: ({ url, turn_port, system_username, system_password } = {}) => ({
     nextcloud_talk_send: {
       configFields: [
@@ -76,39 +124,18 @@ module.exports = {
         },
       ],
       run: async ({ row, user, configuration: { room, text } }) => {
-        const Talk = new NextcloudTalk({
-          server: url,
-          user: system_username,
-          pass: system_password,
-          port: turn_port,
-          debug: true,
-        });
-        Talk.start(500);
+        const the_room = listofrooms.find(
+          (r) => r.name === room || r.displayName === room
+        );
+        if (!the_room) {
+          //console.error(`Room ${room} not found`);
 
-        Talk.on("Ready", (listofrooms) => {
-          // show all details about all rooms of the user
-          //console.log("nctalk rooms", listofrooms);
-          const the_room = listofrooms.find(
-            (r) => r.name === room || r.displayName === room
-          );
-          if (!the_room) {
-            console.error(`Room ${room} not found`);
+          throw new Error(`Room ${room} not found`);
+        } else {
+          let text1 = row ? interpolate(text, row, user) : text;
 
-            //throw new Error(`Room ${room} not found`);
-          } else {
-            let text1 = row ? interpolate(text, row, user) : text;
-
-            Talk.SendMessage(the_room.token, text1);
-          }
-        });
-        Talk.on("Error", (e) => {
-          console.error("Error Event ", e);
-        });
-
-        // Debug
-        Talk.on("Debug", (e) => {
-          console.log("Debug Event ", e);
-        });
+          talk.SendMessage(the_room.token, text1);
+        }
       },
     },
   }),
@@ -118,25 +145,3 @@ module.exports = {
     },
   }),
 };
-/*const onLoad = async (cfg) => {
-  if (!cfg) return;
-  if (!cluster.isMaster) return;
-
-  const { broker_url, subscribe_channels, is_json } = cfg;
-  if (client) await client.end();
-  const broker_url1 = broker_url.includes("://")
-    ? broker_url
-    : `mqtt://${broker_url}`;
-  client = mqtt.connect(broker_url1, { reconnectPeriod: 1000 });
-  client.on("connect", function () {
-    for (channel of subscribe_channels.split(","))
-      client.subscribe(channel.trim());
-  });
-  client.on("message", function (topic, message) {
-    //console.log("MQTT", topic, message);
-    const payload = is_json
-      ? JSON.parse(message.toString())
-      : message.toString();
-    Trigger.emitEvent("MqttReceive", topic, null, payload);
-  });
-};*/
